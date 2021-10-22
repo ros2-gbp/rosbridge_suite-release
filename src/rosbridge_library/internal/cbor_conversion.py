91db93ca81d28767f30fd77c1a14ea5e06ba0cd5
Jacob Bandes-Storch <jacob@foxglove.dev>
2021-10-22 11:27:10 -0700

    Rebase from 'debian/foxy/rosbridge_library'

--- src/rosbridge_library/internal/cbor_conversion.py
+++ src/rosbridge_library/internal/cbor_conversion.py
@@ -1,7 +1,4 @@
 import struct
-import sys
-
-PYTHON2 = sys.version_info < (3, 0)
 
 try:
     from cbor import Tag
@@ -10,27 +7,38 @@ except ImportError:
 
 
 LIST_TYPES = [list, tuple]
-INT_TYPES = ['byte', 'char', 'int8', 'uint8', 'int16', 'uint16', 'int32', 'uint32', 'int64', 'uint64']
-FLOAT_TYPES = ['float32', 'float64']
-STRING_TYPES = ['string']
-BOOL_TYPES = ['bool']
-TIME_TYPES = ['time', 'duration']
-BOOL_ARRAY_TYPES = ['bool[]']
-BYTESTREAM_TYPES = ['uint8[]', 'char[]']
+INT_TYPES = [
+    "byte",
+    "char",
+    "int8",
+    "uint8",
+    "int16",
+    "uint16",
+    "int32",
+    "uint32",
+    "int64",
+    "uint64",
+]
+FLOAT_TYPES = ["float32", "float64"]
+STRING_TYPES = ["string"]
+BOOL_TYPES = ["bool"]
+TIME_TYPES = ["time", "duration"]
+BOOL_ARRAY_TYPES = ["bool[]"]
+BYTESTREAM_TYPES = ["uint8[]", "char[]"]
 
 # Typed array tags according to <https://tools.ietf.org/html/draft-ietf-cbor-array-tags-00>
 # Always encode to little-endian variant, for now.
 TAGGED_ARRAY_FORMATS = {
-    'uint16[]': (69, '<{}H'),
-    'uint32[]': (70, '<{}I'),
-    'uint64[]': (71, '<{}Q'),
-    'byte[]': (72, '{}b'),
-    'int8[]': (72, '{}b'),
-    'int16[]': (77, '<{}h'),
-    'int32[]': (78, '<{}i'),
-    'int64[]': (79, '<{}q'),
-    'float32[]': (85, '<{}f'),
-    'float64[]': (86, '<{}d'),
+    "uint16[]": (69, "<{}H"),
+    "uint32[]": (70, "<{}I"),
+    "uint64[]": (71, "<{}Q"),
+    "byte[]": (72, "{}b"),
+    "int8[]": (72, "{}b"),
+    "int16[]": (77, "<{}h"),
+    "int32[]": (78, "<{}i"),
+    "int64[]": (79, "<{}q"),
+    "float32[]": (85, "<{}f"),
+    "float64[]": (86, "<{}d"),
 }
 
 
@@ -45,12 +53,9 @@ def extract_cbor_values(msg):
     for slot, slot_type in zip(msg.__slots__, msg._slot_types):
         val = getattr(msg, slot)
 
-        if PYTHON2:
-            slot = unicode(slot)
-
         # string
         if slot_type in STRING_TYPES:
-            out[slot] = unicode(val) if PYTHON2 else str(val)
+            out[slot] = str(val)
 
         # bool
         elif slot_type in BOOL_TYPES:
@@ -67,16 +72,13 @@ def extract_cbor_values(msg):
         # time/duration
         elif slot_type in TIME_TYPES:
             out[slot] = {
-                'secs': int(val.secs),
-                'nsecs': int(val.nsecs),
+                "secs": int(val.secs),
+                "nsecs": int(val.nsecs),
             }
 
         # byte array
         elif slot_type in BYTESTREAM_TYPES:
-            if PYTHON2:
-                out[slot] = bytes(bytearray(val))
-            else:
-                out[slot] = bytes(val)
+            out[slot] = bytes(val)
 
         # bool array
         elif slot_type in BOOL_ARRAY_TYPES:
--- src/rosbridge_library/util/__init__.py
+++ src/rosbridge_library/util/__init__.py
@@ -1,22 +1,14 @@
-# try to import json-lib: 1st try ujson, 2nd try simplejson, else import standard python json
+# try to import json-lib: 1st try ujson, 2nd try simplejson, else import standard Python json
 try:
     import ujson as json
 except ImportError:
     try:
         import simplejson as json
     except ImportError:
-        import json
-
-# Differing string types for Python 2 and 3
-import sys
-if sys.version_info >= (3, 0):
-    string_types = (str,)
-    from io import StringIO
-else:
-    string_types = (str, unicode)
-    from StringIO import StringIO
+        import json  # noqa: F401
 
 import bson
+
 try:
     bson.BSON
 except AttributeError:
