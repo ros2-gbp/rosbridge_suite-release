056e63e545fb4abeee38b2a7b8a6496b04a38729
Błażej Sowa <bsowa123@gmail.com>
2025-11-04 22:45:43 +0000

    Rebase from 'upstream'

--- rosbridge_server/test/websocket/smoke.test.py
+++ rosbridge_server/test/websocket/smoke.test.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python
-import os
+from __future__ import annotations
+
 import sys
 import unittest
+from pathlib import Path
+from typing import TYPE_CHECKING
 
-from rclpy.node import Node
 from std_msgs.msg import String
 from twisted.python import log
 
-sys.path.append(os.path.dirname(__file__))  # enable importing from common.py in this directory
+sys.path.append(str(Path(__file__).parent))  # enable importing from common.py in this directory
 
-import common  # noqa: E402
-from common import expect_messages, sleep, websocket_test  # noqa: E402
+import common
+from common import expect_messages, sleep, websocket_test
+
+if TYPE_CHECKING:
+    from collections.abc import Awaitable, Callable
+
+    from common import TestClientProtocol
+    from rclpy.node import Node
 
 log.startLogging(sys.stderr)
 
@@ -19,7 +26,9 @@ generate_test_description = common.generate_test_description
 
 class TestWebsocketSmoke(unittest.TestCase):
     @websocket_test
-    async def test_smoke(self, node: Node, make_client):
+    async def test_smoke(
+        self, node: Node, make_client: Callable[[], Awaitable[TestClientProtocol]]
+    ) -> None:
         ws_client = await make_client()
         # For consistency, the number of messages must not exceed the the protocol
         # Subscriber queue_size.
@@ -37,8 +46,9 @@ class TestWebsocketSmoke(unittest.TestCase):
         ws_completed_future, ws_client.message_handler = expect_messages(
             NUM_MSGS, "WebSocket", node.get_logger()
         )
-        assert node.executor is not None
-        ws_completed_future.add_done_callback(lambda _: node.executor.wake())
+        executor = node.executor
+        assert executor is not None
+        ws_completed_future.add_done_callback(lambda _: executor.wake())
 
         sub_a = node.create_subscription(String, A_TOPIC, sub_handler, NUM_MSGS)
         pub_b = node.create_publisher(String, B_TOPIC, NUM_MSGS)
@@ -77,6 +87,9 @@ class TestWebsocketSmoke(unittest.TestCase):
 
         ros_received = await sub_completed_future
         ws_received = await ws_completed_future
+
+        assert ros_received is not None
+        assert ws_received is not None
 
         for msg in ws_received:
             self.assertEqual("publish", msg["op"])
